c06fc013cf4e9a4247167dc2880ce056c1094e59
Hermes Prazim <hermesbwp@gmail.com>
2020-03-01 20:49:40 -0300

    projeto criado

---
+++ src/main/java/com/hermes/cursomv/CursomvApplication.java
@@ -0,0 +1,13 @@
+package com.hermes.cursomv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomvApplication.class, args);
+	}
+
+}
